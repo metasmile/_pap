712674c998a344c35be8bd50e6df0ec85a43e6d6
metasmile <cyrano905@gmail.com>
2018-03-17 23:53:14 +0100

    does not require default app id

--- batch/Classes/batch.Defaults.swift
+++ batch/Classes/batch.Defaults.swift
@@ -6,33 +6,9 @@
 import Foundation
 import DefaultsKit
 
-public struct CustomStruct: Codable{
-    var customProperty:String = "dd"
-}
-
 extension Defaults: DefaultsAutoProperty {
     public var appIdentifier: String? {
-        set(newValue){ set(newValue) } get{ return get(or:TransformApp.info.identifier) }
-    }
-
-    public var testInt: Int? {
         set(newValue){ set(newValue) } get{ return get() }
-    }
-
-    public var testDate: Date? {
-        set(newValue){ set(newValue) } get{ return get() }
-    }
-
-    public var testCustom: CustomStruct? {
-        set(newValue){ set(newValue, or:CustomStruct()) } get{ return get() }
-    }
-
-    public var testCustom1: CustomStruct? {
-        set(newValue){ set(newValue, or:CustomStruct()) } get{ return get(or:CustomStruct()) }
-    }
-
-    public var testCustom2: CustomStruct {
-        set(newValue){ set(newValue) } get{ return get(or:CustomStruct()) }
     }
 }
 
