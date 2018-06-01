d0a508347b4011fe15a777de79b2ddd5ebfa10df
HYOJIN MO <hyojinmo@gmail.com>
2018-06-01 09:35:06 +0900

    album section

--- pap/Sources/PhotoAlbumViewController.swift
+++ pap/Sources/PhotoAlbumViewController.swift
@@ -11,15 +11,34 @@ import Photos
 
 class PhotoAlbumViewController: UIViewController  {
     @IBOutlet weak var collectionView: UICollectionView!
-    fileprivate var dataSource: PHFetchResult<PHAssetCollection>?
+    fileprivate var dataSource: [[PHAssetCollection]]?
+    private var orderedSmartAlbumSubtypes: [PHAssetCollectionSubtype] = [
+        PHAssetCollectionSubtype.smartAlbumUserLibrary,
+        PHAssetCollectionSubtype.smartAlbumFavorites,
+        PHAssetCollectionSubtype.smartAlbumVideos,
+        PHAssetCollectionSubtype.smartAlbumSelfPortraits,
+        PHAssetCollectionSubtype.smartAlbumLivePhotos,
+        PHAssetCollectionSubtype.smartAlbumDepthEffect,
+        PHAssetCollectionSubtype.smartAlbumPanoramas,
+        PHAssetCollectionSubtype.smartAlbumTimelapses,
+        PHAssetCollectionSubtype.smartAlbumSlomoVideos,
+        PHAssetCollectionSubtype.smartAlbumBursts,
+        PHAssetCollectionSubtype.smartAlbumScreenshots,
+        PHAssetCollectionSubtype.smartAlbumAnimated,
+        PHAssetCollectionSubtype(rawValue: 1000000201)//최근 삭제된 사진
+        ].compactMap { $0 }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let fetchOptions = PHFetchOptions()
+        let smartAlbums = orderedSmartAlbumSubtypes.compactMap {
+            PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: $0, options: nil).firstObject
+        }
         
-        dataSource = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .any, options: fetchOptions)
-//        dataSource = PHAssetCollection.fetchTopLevelUserCollections(with: fetchOptions)
+        let userCollections = PHAssetCollection.fetchTopLevelUserCollections(with: nil)
+        let userAlbums = userCollections.objects(at: IndexSet(integersIn: 0..<userCollections.count)).compactMap { $0 as? PHAssetCollection }
+        
+        dataSource = [smartAlbums, userAlbums].compactMap { $0 }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -31,15 +50,15 @@ class PhotoAlbumViewController: UIViewController  {
 
 extension PhotoAlbumViewController: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return dataSource?.count ?? 0
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataSource?[section].count ?? 0
+    }
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let collection = dataSource?[indexPath.item]
+        let collection = dataSource?[safe: indexPath.section]?[indexPath.item]
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoAlbumCollectionViewCell", for: indexPath) as! PhotoAlbumCollectionViewCell
         cell.titleLabel.text = collection?.localizedTitle
         return cell
@@ -49,7 +68,8 @@ extension PhotoAlbumViewController: UICollectionViewDataSource {
 extension PhotoAlbumViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let photoPickerViewController = R.storyboard.appStoryboard.photoPickerViewController() else { return }
-        photoPickerViewController.collection = dataSource?[indexPath.item]
+        let collection = dataSource?[safe: indexPath.section]?[indexPath.item]
+        photoPickerViewController.collection = collection
         navigationController?.pushViewController(photoPickerViewController, animated: true)
     }
 }
