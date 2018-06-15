94497c80876ea5c59ba4aa6c5c00c7c28b3646c4
metasmile <cyrano905@gmail.com>
2018-06-15 12:33:09 +0200

    purge -> hide

--- pap/Sources/BApp/ExifGhost/ExifGhost.AppDockContent.swift
+++ pap/Sources/BApp/ExifGhost/ExifGhost.AppDockContent.swift
@@ -274,7 +274,7 @@ class ExifGhostAppDockContent: NSObject, AppDockContent, UITableViewDelegate, UI
 
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return section == 0
-                ? "👻 " + "Switch on any items you want to purge. The quality will completely remain the same."
+                ? "👻 " + "Switch on any items you want to hide. The quality will completely remain the same."
                 : metadataCollection[section-1].label
     }
 
@@ -386,7 +386,7 @@ class ExifGhostAppDockContent: NSObject, AppDockContent, UITableViewDelegate, UI
 
         let cell = tableView.dequeueReusableCell(withIdentifier: ExifGhost.info.identifier) as! Cell
         cell.textLabel?.text = dict.items[indexPath.item].label
-        cell.detailTextLabel?.text = selected ? "will be purged" : nil
+        cell.detailTextLabel?.text = selected ? "will be hided" : nil
 //        cell.imageView?.image = selected ? R.image.pdFactoryAppIcon() : nil //selected ? UIImageView(image: R.image.pdFactoryAppIcon()) : nil
         cell.detailTextLabel?.textColor = UIColor.gray
         cell.optionSwitch.setOn(selected, animated: false)
