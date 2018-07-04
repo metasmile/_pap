bc05c2d16f4e593b35f2696bc021c4adbf2415ec
metasmile <cyrano905@gmail.com>
2018-07-04 11:28:34 +0200

    gen10n fix line number duplication

--- genl10n.py
+++ genl10n.py
@@ -56,9 +56,7 @@ for code_file in swift_files:
                     if not str in gened_strs:
                         gened_strs[str] = []
 
-                    if not code_file in gened_strs[str]:
-                        gened_strs[str].append((code_file, i+1))
-
+                    gened_strs[str].append((code_file, i+1))
 
 rcur = codecs.open(dest_l10n_base_path, "r", "utf-8")
 rlines = rcur.readlines()
@@ -94,7 +92,8 @@ for new_key in keys_in_gened_strs:
 
     # gened_strs[new_key][0] : code file path as string
     # gened_strs[new_key][1] : line as int
-    from_files = ", ".join(map(lambda s: "{}#{}".format(os.path.basename(s[0]), s[1]), gened_strs[new_key]))
+    files_indexes_tup = set(gened_strs[new_key]) # remove duplication
+    from_files = ", ".join(map(lambda s: "{}#{}".format(os.path.basename(s[0]), s[1]), files_indexes_tup))
     wlines.append("/* {}: {} */".format(__GEN_FLAG__, from_files))
     wlines.append('\n')
     wlines.append(new_line)
