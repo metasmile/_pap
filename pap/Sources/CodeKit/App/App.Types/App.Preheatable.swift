331e921c523697ff96c92c8731941454986d80b9
metasmile <cyrano905@gmail.com>
2018-07-31 13:41:21 +0200

    add some callback for preheating

--- pap/Sources/CodeKit/App/App.Types/App.Preheatable.swift
+++ pap/Sources/CodeKit/App/App.Types/App.Preheatable.swift
@@ -13,4 +13,17 @@ extension Int: PreheatingFinishAction{}
 
 public protocol PreheatableApp: App{
     func performPreheating(item: PHAssetParamable,  _ async: AsyncWaitSignalable)  -> PreheatingFinishAction?
+
+    //INFO: It will be called at once when definitely cancel.
+    func didCancelPreheating()
+
+    //INFO:
+    // It will be called when finish current perform cycle (for grouped-enqueuing items).
+    // It means that it related with times of calling "performPrefetchIfNeeded". So it can be called N times.
+    func didFinishCurrentPreheatingCycle()
+}
+
+extension PreheatableApp{
+    public func didCancelPreheating(){}
+    public func didFinishCurrentPreheatingCycle(){}
 }
