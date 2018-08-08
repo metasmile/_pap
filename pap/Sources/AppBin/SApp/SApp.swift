901bac3b9f89cdefd61bfe80549b782164d89a95
metasmile <cyrano905@gmail.com>
2018-08-08 11:00:43 +0200

    combine Apps into AppBin group, add SApp group

---
+++ pap/Sources/AppBin/SApp/SApp.swift
@@ -0,0 +1,12 @@
+//
+// Created by BLACKGENE on 20/04/2018.
+// Copyright (c) 2018 Stells. All rights reserved.
+//
+
+import Foundation
+
+/*
+SApp naming basic rule is following BApp
+*/
+
+protocol SApp: App, PersistableApp{}
