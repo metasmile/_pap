1c05530c76f380c6eb39385324d358dea0a835fb
HYOJIN MO <hyojinmo@gmail.com>
2018-08-22 09:03:47 +0900

    fix layout bug

--- pap/Sources/AppUIActionFinalizationViewController.swift
+++ pap/Sources/AppUIActionFinalizationViewController.swift
@@ -138,7 +138,9 @@ internal class ActionFinalizationTableViewCell: UITableViewCell {
         container.addSubview(titleImageView)
         titleImageView.translatesAutoresizingMaskIntoConstraints = false
         titleImageView.widthAnchor.constraint(equalToConstant: 44).isActive = true
-        titleImageView.heightAnchor.constraint(lessThanOrEqualTo: titleImageView.widthAnchor, multiplier: 1).isActive = true
+        let titleImageViewHeightLayout = titleImageView.heightAnchor.constraint(equalTo: titleImageView.widthAnchor, multiplier: 1)
+        titleImageViewHeightLayout.priority = .defaultLow
+        titleImageViewHeightLayout.isActive = true
         titleImageView.topAnchor.constraint(equalTo: container.topAnchor, constant: 0).isActive = true
         titleImageView.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: 0).isActive = true
         titleImageView.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor).isActive = true
