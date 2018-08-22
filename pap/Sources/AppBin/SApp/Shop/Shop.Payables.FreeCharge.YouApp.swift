2ff46220043d61ea1061ce360cc96b33d50d06f5
metasmile <cyrano905@gmail.com>
2018-08-22 20:28:51 +0200

    mod

--- pap/Sources/AppBin/SApp/Shop/Shop.Payables.FreeCharge.YouApp.swift
+++ pap/Sources/AppBin/SApp/Shop/Shop.Payables.FreeCharge.YouApp.swift
@@ -3,15 +3,39 @@
 // Copyright (c) 2018 Stells. All rights reserved.
 //
 
+// https://www.surveymonkey.com/mp/mobile-app-feedback-template/
+
+/*
+
+Mail Address collecting -> Product Hunt Register
+
+*/
+
+/*
+You app Survey
+
+Email
+Twitter ID
+
+How long have you been first app used?
+
+What kind of apps you want? (Multiple Selection) (ex->HiddenRows)
+
+By App Rating
+
+What are PA apps you loved? (Multiple Selection)
+
+Comment (ex->Costomization of rows with text input)
+
+*/
+
 import Foundation
 import Eureka
 import UIKit
 
 class YouAppProgramPayment:NSObject, KeyPathWatchable, PreparablePayable, AdManagerInterestialDelegate{
 
-    required override init() {
-
-    }
+    required override init() {}
 
     private var signal:AsyncWaitSignalable?
 
@@ -30,6 +54,9 @@ class YouAppProgramPayment:NSObject, KeyPathWatchable, PreparablePayable, AdMana
         return "Join".localized
     }
 
+    private lazy var cancelButton:UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(self.cancelButtonDidTap))
+    private lazy var submitButton:UIBarButtonItem = UIBarButtonItem(title: "Submit".localized, style: .done, target: self, action: #selector(self.submitButtonDidTap))
+
     func pay(_ asyncSignal: AsyncWaitSignalable) -> Bool {
 
         var paid = false
@@ -42,15 +69,14 @@ class YouAppProgramPayment:NSObject, KeyPathWatchable, PreparablePayable, AdMana
             formVC.watch(\.wasDone){
                 //TODO: Register to Cloud
                 print(formVC.form.values())
-
                 asyncSignal.end()
             }
 
-            let nVC = UINavigationController(rootViewController: formVC)
-            nVC.title = AppCenter.charge.getCharge(for: type(of: self))?.describable.title
-            nVC.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel".localized, style: .plain, target: self, action: #selector(self.cancelButtonDidTap))
-
-            UIViewController.root!.present(nVC, animated: true)
+            let nVC = PreferredUINavigationController(rootViewController: formVC)
+            nVC.preferredLeftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(self.cancelButtonDidTap))
+            nVC.preferredRightBarButtonItem = UIBarButtonItem(title: "Submit".localized, style: .done, target: self, action: #selector(self.submitButtonDidTap))
+            nVC.preferredTitle = AppCenter.charge.getCharge(for: type(of: self))?.describable.title
+            UIViewController.root?.present(nVC, animated: true)
         }
         asyncSignal.waitUntilEnd()
 
@@ -60,9 +86,42 @@ class YouAppProgramPayment:NSObject, KeyPathWatchable, PreparablePayable, AdMana
     @objc func cancelButtonDidTap(sender: Any) {
         while signal?.end().began ?? false {}
     }
+
+    @objc func submitButtonDidTap(sender: Any) {
+        while signal?.end().began ?? false {}
+    }
 }
 
-class YouAppFormController: FormViewController, KeyPathWatchable {
+private class PreferredUINavigationController: UINavigationController{
+    var preferredLeftBarButtonItem:UIBarButtonItem?
+    var preferredRightBarButtonItem:UIBarButtonItem?
+    var preferredTitle:String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //TODO why not working??
+
+        self.navigationItem.title = AppCenter.charge.getCharge(for: YouAppProgramPayment.self)?.describable.title
+        self.title = AppCenter.charge.getCharge(for: YouAppProgramPayment.self)?.describable.title
+
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(self.cancelButtonDidTap))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Submit".localized, style: .done, target: self, action: #selector(self.submitButtonDidTap))
+
+        self.navigationBar.topItem?.setLeftBarButton(preferredLeftBarButtonItem, animated: true)
+        self.navigationBar.topItem?.setRightBarButton(preferredRightBarButtonItem, animated: true)
+        self.navigationBar.topItem?.title = preferredTitle
+    }
+
+    @objc func cancelButtonDidTap(sender: Any) {
+     }
+
+    @objc func submitButtonDidTap(sender: Any) {
+     }
+}
+
+
+private class YouAppFormController: FormViewController, KeyPathWatchable {
 
     @objc dynamic
     var wasDone = false
@@ -85,19 +144,17 @@ class YouAppFormController: FormViewController, KeyPathWatchable {
         }
 
         form
-        +++ Section(header: "Required Rule", footer: "Options: Validates on change")
+        +++ Section(header: "Your Information", footer: "")
 
         <<< TextRow() {
-            $0.title = "Required Rule"
+            $0.title = "Name".localized
             $0.add(rule: RuleRequired())
+            $0.add(rule: RuleMinLength(minLength: 3))
             $0.validationOptions = .validatesOnChange
         }
 
-
-        +++ Section(header: "Email Rule, Required Rule", footer: "Options: Validates on change after blurred")
-
         <<< TextRow() {
-            $0.title = "Email Rule"
+            $0.title = "Email Address".localized
             $0.add(rule: RuleRequired())
             var ruleSet = RuleSet<String>()
             ruleSet.add(rule: RuleRequired())
@@ -106,60 +163,60 @@ class YouAppFormController: FormViewController, KeyPathWatchable {
             $0.validationOptions = .validatesOnChangeAfterBlurred
         }
 
-        +++ Section(header: "URL Rule", footer: "Options: Validates on change")
-
-        <<< URLRow() {
-            $0.title = "URL Rule"
-            $0.add(rule: RuleURL())
-            $0.validationOptions = .validatesOnChange
-        }
-                .cellUpdate { cell, row in
-                    if !row.isValid {
-                        cell.titleLabel?.textColor = .red
-                    }
-                }
-
-
-        +++ Section(header: "MinLength 8 Rule, MaxLength 13 Rule", footer: "Options: Validates on blurred")
-        <<< PasswordRow() {
-            $0.title = "Password"
-            $0.add(rule: RuleMinLength(minLength: 8))
-            $0.add(rule: RuleMaxLength(maxLength: 13))
-        }
-                .cellUpdate { cell, row in
-                    if !row.isValid {
-                        cell.titleLabel?.textColor = .red
-                    }
-                }
+//        +++ Section(header: "URL Rule", footer: "Options: Validates on change")
+//
+//        <<< URLRow() {
+//            $0.title = "URL Rule"
+//            $0.add(rule: RuleURL())
+//            $0.validationOptions = .validatesOnChange
+//        }
+//                .cellUpdate { cell, row in
+//                    if !row.isValid {
+//                        cell.titleLabel?.textColor = .red
+//                    }
+//                }
 
 
-        +++ Section(header: "Should be GreaterThan 2 and SmallerThan 999", footer: "Options: Validates on blurred")
+//        +++ Section(header: "MinLength 8 Rule, MaxLength 13 Rule", footer: "Options: Validates on blurred")
+//        <<< PasswordRow() {
+//            $0.title = "Password"
+//            $0.add(rule: RuleMinLength(minLength: 8))
+//            $0.add(rule: RuleMaxLength(maxLength: 13))
+//        }
+//                .cellUpdate { cell, row in
+//                    if !row.isValid {
+//                        cell.titleLabel?.textColor = .red
+//                    }
+//                }
 
-        <<< IntRow() {
-            $0.title = "Range Rule"
-            $0.add(rule: RuleGreaterThan(min: 2))
-            $0.add(rule: RuleSmallerThan(max: 999))
-        }
-                .cellUpdate { cell, row in
-                    if !row.isValid {
-                        cell.titleLabel?.textColor = .red
-                    }
-                }
 
-        +++ Section(header: "Match field values", footer: "Options: Validates on blurred")
+//        +++ Section(header: "Should be GreaterThan 2 and SmallerThan 999", footer: "Options: Validates on blurred")
+//
+//        <<< IntRow() {
+//            $0.title = "Range Rule"
+//            $0.add(rule: RuleGreaterThan(min: 2))
+//            $0.add(rule: RuleSmallerThan(max: 999))
+//        }
+//                .cellUpdate { cell, row in
+//                    if !row.isValid {
+//                        cell.titleLabel?.textColor = .red
+//                    }
+//                }
 
-        <<< PasswordRow("password") {
-            $0.title = "Password"
-        }
-        <<< PasswordRow() {
-            $0.title = "Confirm Password"
-            $0.add(rule: RuleEqualsToRow(form: form, tag: "password"))
-        }
-                .cellUpdate { cell, row in
-                    if !row.isValid {
-                        cell.titleLabel?.textColor = .red
-                    }
-                }
+//        +++ Section(header: "Match field values", footer: "Options: Validates on blurred")
+//
+//        <<< PasswordRow("password") {
+//            $0.title = "Password"
+//        }
+//        <<< PasswordRow() {
+//            $0.title = "Confirm Password"
+//            $0.add(rule: RuleEqualsToRow(form: form, tag: "password"))
+//        }
+//                .cellUpdate { cell, row in
+//                    if !row.isValid {
+//                        cell.titleLabel?.textColor = .red
+//                    }
+//                }
 
 
         +++ Section(header: "More sophisticated validations UX using callbacks", footer: "")
@@ -335,11 +392,11 @@ class YouAppFormController: FormViewController, KeyPathWatchable {
         }
                 .onCellSelection { cell, row in
                     
-//                    if row.section?.form?.validate().count == 0{
+                    if row.section?.form?.validate().count == 0{
                         (self.navigationController ?? self).dismiss(animated: true, completion: {
                             self.wasDone = true
                         })
-//                    }
+                    }
                     
                 }
 
