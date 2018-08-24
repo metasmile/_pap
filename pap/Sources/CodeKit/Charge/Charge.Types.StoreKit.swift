5acb19a957ae9610f69d6a6b7859be6dc12b50ef
metasmile <cyrano905@gmail.com>
2018-08-24 15:09:29 +0200

    add trial conception to StorePayable

--- pap/Sources/CodeKit/Charge/Charge.Types.StoreKit.swift
+++ pap/Sources/CodeKit/Charge/Charge.Types.StoreKit.swift
@@ -5,6 +5,7 @@
 
 import Foundation
 import StoreKit
+import DefaultsKit
 
 protocol StorePayable: VerifiablePayable {
     static var product: StoreProduct {get}
@@ -17,4 +18,55 @@ protocol StorePayable: VerifiablePayable {
 struct StoreProduct {
     let identifier:String
     let subscriptionPeriod: Period?
+    let trialTimeLength:TimeInterval?
+
+    init(identifier:String, subscriptionPeriod:Period?, trialTimeLength:TimeInterval?=nil){
+        self.identifier = identifier
+        self.subscriptionPeriod = subscriptionPeriod
+        self.trialTimeLength = trialTimeLength
+    }
+}
+
+/*
+    Trial Control
+*/
+extension StoreProduct{
+    private var ExpiredTrialLength:TimeInterval{
+        return -Double.greatestFiniteMagnitude
+    }
+
+    func activateTrialIfNeeded(){
+        assert(trialTimeLength != nil, "This product not configured trialTimeLength")
+        assert(Defaults.shared.remainingTrialPeriod[identifier] == nil, "Already activated trial")
+        assert(Defaults.shared.remainingTrialPeriod[identifier] != ExpiredTrialLength, "Expired")
+
+        let t = Defaults.shared.remainingTrialPeriod[identifier]
+        if let trialLength = trialTimeLength, t == nil, t != ExpiredTrialLength{
+            Defaults.shared.remainingTrialPeriod[identifier] = trialLength
+        }
+    }
+
+    func expireTrialIfNeeded(){
+        assert(remainingTrialPeriod != nil, "already invalidated")
+        if remainingTrialPeriod != nil{
+            Defaults.shared.remainingTrialPeriod[identifier] = ExpiredTrialLength
+        }
+    }
+
+    var remainingTrialPeriod:TimeInterval? {
+        if let _ = trialTimeLength, let t = Defaults.shared.remainingTrialPeriod[identifier]{
+            return t != ExpiredTrialLength ? t : nil
+        }
+        return nil
+    }
+}
+
+private protocol StoreProductInternalDefaults:DefaultsProperty{
+    var remainingTrialPeriod:[String:TimeInterval] {set get}
+}
+
+extension Defaults: StoreProductInternalDefaults {
+    var remainingTrialPeriod: [String:TimeInterval] {
+        set{ set(newValue) } get{ return get(or:[String:TimeInterval]()) }
+    }
 }
