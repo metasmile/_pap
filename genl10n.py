7e6d44a5120039b13344fcee59afb3de83b54e62
metasmile <cyrano905@gmail.com>
2018-08-30 18:47:04 +0200

    exclude commented strings

--- genl10n.py
+++ genl10n.py
@@ -39,6 +39,8 @@ complied_patterns_by_priority = [
 
 # for excluing format literal -> \(value)
 qs = re.compile(r'\\\((.+)\)', re.I|re.U)
+# for excluing code comment
+cs = re.compile(r'\/\/.*', re.I|re.U)
 
 swift_files = []
 
@@ -51,6 +53,10 @@ for code_file in swift_files:
     rcur = codecs.open(code_file, "r", "utf-8")
     wlines = []
     for i, line in enumerate(rcur.readlines()):
+
+        if cs.search(line):
+            line = cs.sub("", line)
+
         for line_sp in line.split(split_key):
 
             for p in complied_patterns_by_priority:
