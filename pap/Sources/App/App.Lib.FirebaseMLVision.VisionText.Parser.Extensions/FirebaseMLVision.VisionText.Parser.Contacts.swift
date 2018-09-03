4bd043dd499164db2cefa3433fb40bdb0bc873b9
metasmile <cyrano905@gmail.com>
2018-09-03 13:14:09 +0200

    [FIX] Avoid a case of starting with '-'

--- pap/Sources/App/App.Lib.FirebaseMLVision.VisionText.Parser.Extensions/FirebaseMLVision.VisionText.Parser.Contacts.swift
+++ pap/Sources/App/App.Lib.FirebaseMLVision.VisionText.Parser.Extensions/FirebaseMLVision.VisionText.Parser.Contacts.swift
@@ -55,7 +55,7 @@ public struct VisionTextPhoneNumberParser: VisionTextParser{
     private let blockParser = VisionTextTextBlockParser()
 
     //https://en.wikipedia.org/wiki/National_conventions_for_writing_telephone_numbers
-    private let deniedPattern = "[^0-9\\+\\s\\)\\(\\-]"
+    private let deniedPattern = "[^0-9\\+\\s\\)\\(\\-]|(^\\-)"
 
     func process(input: FirebaseMLVision.VisionText) -> VisionTextStringElementsParser.OutputType? {
         guard let lines = blockParser.process(input: input) else{
