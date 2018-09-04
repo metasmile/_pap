d9ff546c65bbfd272f8505b7ca6d990d3b0460cb
metasmile <cyrano905@gmail.com>
2018-09-04 10:30:06 +0200

    [POLICY] 2nd Lockscreen

--- pap/Sources/AppBin/BApp/Cleaner/Cleaner.BApp.swift
+++ pap/Sources/AppBin/BApp/Cleaner/Cleaner.BApp.swift
@@ -97,9 +97,9 @@ public class CleanerApp: NSObject, BApp, PropertyWatchable, LaunchableApp, PHAss
     fileprivate static var DefaultEnabledGDTypes:[PHAssetGarbageDetector.Type]{
         return [
             PHAssetGarbageDetector_Similarity.self
+            , PHAssetGarbageDetector_Lockscreens.self
             , PHAssetGarbageDetector_VideosSavedbyInstagramApp.self
             , PHAssetGarbageDetector_Flashlight.self
-            , PHAssetGarbageDetector_VideosShorterThan1Sec.self
         ]
     }
 
