392d6d5c38a00e9d2b3e9f5e678007a984f044cd
metasmile <cyrano905@gmail.com>
2018-09-13 13:06:17 +0200

    rm legacy 'func reset()'

--- pap/Sources/CodeKit/CodeKit.UIView.GestureRecognizer.swift
+++ pap/Sources/CodeKit/CodeKit.UIView.GestureRecognizer.swift
@@ -83,18 +83,7 @@ class DragSelectionGestureRecognizer: UIPanGestureRecognizer {
     var selectionMode = DragSelectionGestureRecognizer.DragSelectionMode.none
     var autoPanningTimer: CADisplayLink?
 
-    //TODO: ugh... remove this line when able to use xcode10 for appstore release build .
-#if DEBUG //DEBUG means sdk 12, otherwise is sdk 11.
     override func reset() {
-        super.reset()
-        _reset()
-    }
-#else
-    func reset() {
-        _reset()
-    }
-#endif
-    func _reset(){
         beginIndexPath = nil
         beginLocation = nil
         ignoredIndexPaths = nil
@@ -103,7 +92,7 @@ class DragSelectionGestureRecognizer: UIPanGestureRecognizer {
         selectionMode = .none
         stopAutoPanning()
     }
-    
+
     private var panHandler: (() -> Void)?
     func panAutomatically(_ panBlock: (() -> Void)?) {
         if autoPanningTimer == nil {
