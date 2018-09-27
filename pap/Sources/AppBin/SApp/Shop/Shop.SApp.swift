c5dfa11f25c52283bc64c719bf007bcc819d71b9
metasmile <cyrano905@gmail.com>
2018-09-27 14:41:25 +0200

    Video Tutorials

--- pap/Sources/AppBin/SApp/Shop/Shop.SApp.swift
+++ pap/Sources/AppBin/SApp/Shop/Shop.SApp.swift
@@ -697,12 +697,12 @@ fileprivate class ShopAppDockContent: NSObject, AppDockContent, UITableViewDeleg
 
         let c345 = UITableViewButtonCellDescriber()
         c345.itemIdentifier = CellDescriber.Key.support.hashValue
-        c345.label = "%@ Channel".localizedFormatted("YouTube")
+        c345.label = "Video Tutorials".localized
         c345.buttonTitle = "Visit".localized
         c345.iconImage = R.image.cellIconYouTubeChannel.name
         c345.iconImageTintColor = self.view.tintColor
         c345.valueHandler = { _ in
-            AppCenter.charge.try(for: URLOpenPayment<URLOpenTypeYouTubeChannel>.self)
+            AppCenter.charge.try(for: URLOpenPayment<URLOpenTypeVideoTutorials>.self)
         }
         contactCellDescribers.append(c345)
     }
