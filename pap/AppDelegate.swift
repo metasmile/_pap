8739183be12f844fa9fac5831e9eecd0848d1d35
metasmile <cyrano905@gmail.com>
2018-10-06 13:40:01 +0200

    str + rm warning

--- pap/AppDelegate.swift
+++ pap/AppDelegate.swift
@@ -81,7 +81,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    private func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         spotlightSearchAppDelegate.application(application, userActivity: userActivity, restorationHandler: restorationHandler)
         intentsAppDelegate.application(application, userActivity: userActivity, restorationHandler: restorationHandler)
         return true
