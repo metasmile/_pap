42439332cad03fe9193ffe1ce31d2b35b0cf43fd
metasmile <cyrano905@gmail.com>
2018-10-11 17:25:43 +0200

    resize based cgimage

--- pap/Sources/CodeKit/CodeKit.UIImage.swift
+++ pap/Sources/CodeKit/CodeKit.UIImage.swift
@@ -81,38 +81,10 @@ public extension UIImage {
 
     //INFO: Use this default
     func re(size:CGSize) -> UIImage? {
-        let cgImage = self.cgImage!
-
-        var format = vImage_CGImageFormat(bitsPerComponent: 8, bitsPerPixel: 32, colorSpace: nil,
-                bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.first.rawValue),
-                version: 0, decode: nil, renderingIntent: CGColorRenderingIntent.defaultIntent)
-        var sourceBuffer = vImage_Buffer()
-        defer {
-            sourceBuffer.data.deallocate()
+        if let resizedCgImage = self.cgImage?.re(size: size){
+            return UIImage(cgImage: resizedCgImage, scale: self.scale, orientation: self.imageOrientation)
         }
-
-        var error = vImageBuffer_InitWithCGImage(&sourceBuffer, &format, nil, cgImage, numericCast(kvImageNoFlags))
-        guard error == kvImageNoError else { return nil }
-
-        let scale = self.scale
-        let destWidth = Int(size.width)
-        let destHeight = Int(size.height)
-        let bytesPerPixel = cgImage.bitsPerPixel / 8
-        let destBytesPerRow = destWidth * bytesPerPixel
-        let destData = UnsafeMutablePointer<UInt8>.allocate(capacity: destHeight * destBytesPerRow)
-        defer {
-            destData.deallocate()
-        }
-        var destBuffer = vImage_Buffer(data: destData, height: vImagePixelCount(destHeight), width: vImagePixelCount(destWidth), rowBytes: destBytesPerRow)
-
-        error = vImageScale_ARGB8888(&sourceBuffer, &destBuffer, nil, numericCast(kvImageHighQualityResampling))
-        guard error == kvImageNoError else { return nil }
-
-        let destCGImage = vImageCreateCGImageFromBuffer(&destBuffer, &format, nil, nil, numericCast(kvImageNoFlags), &error)?.takeRetainedValue()
-        guard error == kvImageNoError else { return nil }
-
-        let resizedImage = destCGImage.flatMap { UIImage(cgImage: $0, scale: 0.0, orientation: self.imageOrientation) }
-        return resizedImage
+        return nil
     }
 
 
