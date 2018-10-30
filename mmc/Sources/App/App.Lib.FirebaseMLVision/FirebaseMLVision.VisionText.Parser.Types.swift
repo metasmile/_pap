ac93eb05e4de2e6dca26115c0402ea40643a13cd
HYOJIN MO <hyojinmo@gmail.com>
2018-10-31 08:05:34 +0900

    hmm strict currency fraction digits 2

--- mmc/Sources/App/App.Lib.FirebaseMLVision/FirebaseMLVision.VisionText.Parser.Types.swift
+++ mmc/Sources/App/App.Lib.FirebaseMLVision/FirebaseMLVision.VisionText.Parser.Types.swift
@@ -317,10 +317,10 @@ public struct VisionTextCurrencyParser: VisionTextParser{
     private static let currencySymbolRegexPattern = "\\p{Currency_Symbol}"
     private static let currencyCodeRegexPattern = "[A-Z]{3}"
     
-    private static let commaGroupSeparatorRegexPattern = "[+-]?[0-9]+(?:,?[0-9]{3})*(?:.?[0-9]+)?"
-    private static let dotGroupSeparatorRegexPattern = "[+-]?[0-9]+(?:.?[0-9]{3})*(?:,?[0-9]+)?"
-    private static let spaceGroupSeparatorRegexPattern = "[+-]?[0-9]+(?:\\s?[0-9]{3})*(?:.?[0-9]+|,?[0-9]+)?"
-    private static let priceRegexPattern = "[+-]?[0-9]+(?:,?[0-9]{3}|.?[0-9]{3})*(?:.?[0-9]+|,?[0-9]+)?"
+    private static let commaGroupSeparatorRegexPattern = "[+-]?[0-9]+(?:,?[0-9]{3})*(?:.?[0-9]{2})?"
+    private static let dotGroupSeparatorRegexPattern = "[+-]?[0-9]+(?:.?[0-9]{3})*(?:,?[0-9]{2})?"
+    private static let spaceGroupSeparatorRegexPattern = "[+-]?[0-9]+(?:\\s?[0-9]{3})*(?:.?[0-9]{2}|,?[0-9]{2})?"
+    private static let priceRegexPattern = "[+-]?[0-9]+(?:,?[0-9]{3}|.?[0-9]{3})*(?:.?[0-9]{2}|,?[0-9]{2})?"
     
     private static let currencyRegexPatternType1 = "\(currencySymbolRegexPattern)\\s*\(priceRegexPattern)"
     private static let currencyRegexPatternType2 = "\(priceRegexPattern)\\s*\(currencySymbolRegexPattern)"
@@ -366,11 +366,11 @@ public struct VisionTextCurrencyParser: VisionTextParser{
                     }
                 }
                 
-                if match.matched(",[0-9]+$") {
+                if match.matched(",[0-9]{2}$") {
                     formatter.currencyGroupingSeparator = "."
                     formatter.currencyDecimalSeparator = ","
                 }
-                else if match.matched(".[0-9]+$") {
+                else if match.matched(".[0-9]{2}$") {
                     formatter.currencyGroupingSeparator = ","
                     formatter.currencyDecimalSeparator = "."
                 }
@@ -379,8 +379,8 @@ public struct VisionTextCurrencyParser: VisionTextParser{
                 
                 priceString = priceString.replaceIfMatched(withPattern: "\\s", replace: "")
                 
-                if let fractionString = priceString.matchedStrings(",[0-9]+$").first {
-                    priceString = priceString.replaceIfMatched(withPattern: ",[0-9]+$", replace: fractionString.replace(",", "."))
+                if let fractionString = priceString.matchedStrings(",[0-9]{2}$").first {
+                    priceString = priceString.replaceIfMatched(withPattern: ",[0-9]{2}$", replace: fractionString.replace(",", "."))
                 }
                 
                 let decimalFormatter = NumberFormatter()
