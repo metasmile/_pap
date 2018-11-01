51c996964ae1ac89e6e82a77749203ac93c16ff7
metasmile <cyrano905@gmail.com>
2018-11-01 23:25:17 +0100

    l10n gen

--- genl10n.py
+++ genl10n.py
@@ -17,8 +17,8 @@ import collections
 parser = argparse.ArgumentParser(
     description='Extract elements from from Swift code to the .strings file.')
 
-parser.add_argument('src_path', help='Main swift source path, default=./)',
-                    default='./', nargs='?')
+parser.add_argument('src_paths', help='Main swift source path, default="./")',
+                    default='./', type=str, nargs='?')
 parser.add_argument('dest_l10n_base_path', help='Target Base Localizable.strings path. (default=./)',
                     default='./', nargs='?')
 parser.add_argument('-k', '--split-key',
@@ -26,7 +26,8 @@ parser.add_argument('-k', '--split-key',
                     default='.localized', required=False)
 args = vars(parser.parse_args())
 
-src_path = expanduser(args['src_path'])
+src_paths = [expanduser(path) for path in args['src_paths'].split(" ")]
+
 dest_l10n_base_path = expanduser(args['dest_l10n_base_path'])
 split_key = args['split_key']
 
@@ -44,9 +45,10 @@ cs = re.compile(r'\/\/.*', re.I|re.U)
 
 swift_files = []
 
-for root, dirnames, filenames in os.walk(src_path):
-    for filename in fnmatch.filter(filenames, '*.swift'):
-        swift_files.append(os.path.join(root, filename))
+for src_path in src_paths:
+    for root, dirnames, filenames in os.walk(src_path):
+        for filename in fnmatch.filter(filenames, '*.swift'):
+            swift_files.append(os.path.join(root, filename))
 
 gened_strs = collections.OrderedDict()
 for code_file in swift_files:
@@ -75,9 +77,9 @@ for code_file in swift_files:
                         continue
 
                     if not str in gened_strs:
-                        gened_strs[str] = []
+                        gened_strs[str] = set()
 
-                    gened_strs[str].append((code_file, i+1))
+                    gened_strs[str].add((code_file, i+1))
                     break # if pattern was found by one of them, exit loop
 
 rcur = codecs.open(dest_l10n_base_path, "r", "utf-8")
