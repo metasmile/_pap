c817318e7c31ae2e436f10e3cc8c298ffad044de
metasmile <cyrano905@gmail.com>
2018-11-05 11:43:29 +0100

    app trial 1 day

--- batch/Sources/batch.TimerInterval.swift
+++ batch/Sources/batch.TimerInterval.swift
@@ -29,7 +29,7 @@ struct batchTimeInterval {
             return 30
         }
 #endif
-        return 60*60*24*3
+        return 60*60*24*1
     }
 
     static var ofGADInterestialTypeBlockOfUses:TimeInterval{
