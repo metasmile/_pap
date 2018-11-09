9aad5e5053899df0bd1227dcb747be1d113e2cb8
HYOJIN MO <hyojinmo@gmail.com>
2018-11-09 10:02:35 +0900

    add clipboard icon

---
+++ batch/Sources/AppBin/BApp/Resizer/Resizer.BApp.swift
@@ -0,0 +1,13 @@
+//
+//  ResizerBApp.swift
+//  batch
+//
+//  Created by HYOJIN MO on 09/11/2018.
+//  Copyright © 2018 Stells. All rights reserved.
+//
+
+import UIKit
+
+class ResizerBApp: NSObject {
+
+}
