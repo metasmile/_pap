6ccf5b44083dceb4f5cd188dc6d45798062c0b8a
HYOJIN MO <hyojinmo@gmail.com>
2018-11-12 15:48:51 +0900

    add resizer (not completed for video yet)

--- batch/Sources/AppCenter.swift
+++ batch/Sources/AppCenter.swift
@@ -60,6 +60,7 @@ public final class AppCenter: AppManager, AppManagerConfigurable, PropertyWatcha
             , ExifGhostApp.self
             , SiriApp.self
             , ClipboardApp.self
+            , ResizerApp.self
 //            , Stabilizer.self
 
         ].sorted { (appType1: App.Type, appType2: App.Type) -> Bool in
