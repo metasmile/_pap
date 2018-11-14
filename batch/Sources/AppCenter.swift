fc976ba9e05ad648d186d522252a4bfe37e5a434
metasmile <cyrano905@gmail.com>
2018-11-14 11:34:47 +0100

    SEP Appcenter.configurage

--- batch/Sources/AppCenter.swift
+++ batch/Sources/AppCenter.swift
@@ -6,7 +6,7 @@
 import Foundation
 import PropertyKit
 
-public final class AppCenter: AppManager, AppManagerConfigurable, PropertyWatchable {
+public final class AppCenter: AppManager, PropertyWatchable {
     public static let `default` = AppCenter()
 
     override init() {
@@ -36,66 +36,5 @@ public final class AppCenter: AppManager, AppManagerConfigurable, PropertyWatcha
 
     private var initialApp:App.Type{
         return TransformApp.self
-    }
-
-    func configure() -> AppManagerConfig? {
-
-        var config = AppManagerConfig()
-
-        let defaultAppCollection:[App.Type] = [
-            TransformApp.self
-            , FinderApp.self
-            , FiltersApp.self
-            , ConverterApp.self
-            , ArtistApp.self
-            , CleanerApp.self
-            , MemoCamApp.self
-            , GIFMakerApp.self
-            , RevertApp.self
-            , PDFMakerApp.self
-            , CameraApp.self
-            , ShopApp.self
-            , SettingsApp.self
-            , AutoEditorApp.self
-            , ExifGhostApp.self
-            , SiriApp.self
-            , ClipboardApp.self
-            , ResizerApp.self
-//            , Stabilizer.self
-
-        ].sorted { (appType1: App.Type, appType2: App.Type) -> Bool in
-
-            if appType1.info.phase.rawValue > appType2.info.phase.rawValue{
-                return true
-            }
-
-            if papCount.app.countPerformed(app: appType1) > papCount.app.countPerformed(app: appType2){
-                return true
-            }
-
-            if appType1 is SApp.Type && appType2 is BApp.Type {
-                return true
-            }
-
-            if appType1 is AVCaptureDeviceApp.Type == false && appType2 is AVCaptureDeviceApp.Type{
-                return true
-            }
-
-            return false
-        }
-
-        config.appCollection = defaultAppCollection
-
-        print("[i] App Internal Collection: ",defaultAppCollection)
-
-        #if DEBUG
-        for app in defaultAppCollection{
-            print(app.info.displayName)
-            print(app.info.description ?? "")
-//            print(app.info.keywords?.joined(separator: ",") ?? "")
-        }
-        #endif
-
-        return config
     }
 }
