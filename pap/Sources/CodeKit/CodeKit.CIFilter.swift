c9cf8465337f461636781b32851a40848147caa9
HYOJIN MO <hyojinmo@gmail.com>
2018-12-24 14:46:34 +0900

    test for color cube

--- pap/Sources/CodeKit/CodeKit.CIFilter.swift
+++ pap/Sources/CodeKit/CodeKit.CIFilter.swift
@@ -99,3 +99,125 @@ public class CIFilterAttributes {
         }
     }
 }
+
+// https://github.com/muukii/ColorCube/blob/master/ColorCube/ColorCube.swift
+public class CIColorCube: CIFilter {
+    private var lutImage: UIImage?
+    private var dimension: Int = 64
+    
+    convenience init(lut image: UIImage?, dimension: Int = 64) {
+        self.init()
+        
+        self.lutImage = image
+        self.dimension = dimension
+    }
+    
+    @objc dynamic var inputImage: CIImage?
+    
+    public override var outputImage: CIImage? {
+        return autoreleasepool { () -> CIImage? in
+            guard let inputImage = inputImage, let lutImage = lutImage, let cubeData = CIColorCube.cubeData(lutImage: lutImage, dimension: dimension, colorSpace: inputImage.defaultColorSpace) else { return nil }
+            let filter = CIFilter(name: "CIColorCube", parameters: [
+                kCIInputImageKey: inputImage,
+                "inputCubeData": cubeData,
+                "inputCubeDimension": dimension
+            ])
+            return filter?.outputImage
+        }
+    }
+    
+    private static func createBitmap(image: CGImage, colorSpace: CGColorSpace) -> UnsafeMutablePointer<UInt8>? {
+        let width = image.width
+        let height = image.height
+        
+        let bitsPerComponent = 8
+        let bytesPerRow = width * 4
+        
+        let bitmapSize = bytesPerRow * height
+        
+        guard let data = malloc(bitmapSize) else {
+            return nil
+        }
+        
+        guard let context = CGContext(
+            data: data,
+            width: width,
+            height: height,
+            bitsPerComponent: bitsPerComponent,
+            bytesPerRow: bytesPerRow,
+            space: colorSpace,
+            bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue,
+            releaseCallback: nil,
+            releaseInfo: nil) else {
+                return nil
+        }
+        
+        context.draw(image, in: CGRect(x: 0, y: 0, width: width, height: height))
+        
+        return data.bindMemory(to: UInt8.self, capacity: bitmapSize)
+    }
+    
+    private static func cubeData(lutImage: UIImage, dimension: Int, colorSpace: CGColorSpace) -> Data? {
+        
+        guard let cgImage = lutImage.cgImage else {
+            return nil
+        }
+        
+        guard let bitmap = createBitmap(image: cgImage, colorSpace: colorSpace) else {
+            return nil
+        }
+        
+        let width = cgImage.width
+        let height = cgImage.height
+        let rowNum = width / dimension
+        let columnNum = height / dimension
+        
+        let dataSize = dimension * dimension * dimension * MemoryLayout<Float>.size * 4
+        
+        var array = [Float](repeating: 0, count: dataSize)
+        
+        var bitmapOffest: Int = 0
+        var z: Int = 0
+        
+        for _ in stride(from: 0, to: rowNum, by: 1) {
+            for y in stride(from: 0, to: dimension, by: 1) {
+                let tmp = z
+                for _ in stride(from: 0, to: columnNum, by: 1) {
+                    for x in stride(from: 0, to: dimension, by: 1) {
+                        
+                        let dataOffset = (z * dimension * dimension + y * dimension + x) * 4
+                        
+                        let position = bitmap
+                            .advanced(by: bitmapOffest)
+                        
+                        array[dataOffset + 0] = Float(position
+                            .advanced(by: 0)
+                            .pointee) / 255
+                        
+                        array[dataOffset + 1] = Float(position
+                            .advanced(by: 1)
+                            .pointee) / 255
+                        
+                        array[dataOffset + 2] = Float(position
+                            .advanced(by: 2)
+                            .pointee) / 255
+                        
+                        array[dataOffset + 3] = Float(position
+                            .advanced(by: 3)
+                            .pointee) / 255
+                        
+                        bitmapOffest += 4
+                        
+                    }
+                    z += 1
+                }
+                z = tmp
+            }
+            z += columnNum
+        }
+        
+        free(bitmap)
+        
+        return array.withUnsafeBufferPointer { Data(buffer: $0) }
+    }
+}
