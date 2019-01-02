16a87ee19e39fb35b1eada47fdc331ba4eaee6a9
metasmile <cyrano905@gmail.com>
2019-01-02 20:30:38 +0100

    strs

--- batch/Sources/AppBin/SApp/Shop/Payments/Shop.Payment.Promotion.swift
+++ batch/Sources/AppBin/SApp/Shop/Payments/Shop.Payment.Promotion.swift
@@ -32,7 +32,6 @@ private struct AppStoreRatingInitializer{
     fileprivate static func initialize(){
         Armchair.appID(InfoStrings.appStoreId)
         Armchair.useStoreKitReviewPrompt(true)
-        Armchair.resetAllCounters()
         Armchair.shouldIncrementUseCountClosure { () -> Bool in
             return false
         }
