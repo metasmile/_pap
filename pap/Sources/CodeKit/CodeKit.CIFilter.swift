26f6d3ad04f70550d1e6532d47d0ada3334bbc1a
HYOJIN MO <hyojinmo@gmail.com>
2019-03-07 13:45:29 +0900

    separate filters

--- pap/Sources/CodeKit/CodeKit.CIFilter.swift
+++ pap/Sources/CodeKit/CodeKit.CIFilter.swift
@@ -196,22 +196,6 @@ extension CIFilterAttributes: Equatable {
     }
 }
 
-class CIAdjustmentSliderInfo {
-    var name: String
-    var attributeKey: String
-    var range: ClosedRange<Float>?
-    
-    init(name: String, attributeKey: String, range: ClosedRange<Float>? = nil, userAttributeItems: [CIFilterAttributeItem]? = nil) {
-        self.name = name
-        self.attributeKey = attributeKey
-        self.range = range
-        
-        self.userAttributeItems = userAttributeItems
-    }
-    
-    var userAttributeItems: [CIFilterAttributeItem]?
-}
-
 class CIBuiltInFilter: CIFilter, Codable {
     var filterAttributes = [String: CIFilterAttributes]()
     private var builtInFilter: CIFilter?
@@ -267,73 +251,6 @@ class CIBuiltInFilter: CIFilter, Codable {
 
     var hasChanges: Bool {
         return filterAttributes.values.reduce(false) { $0 || $1.hasChanges }
-    }
-    
-    override func isEqual(_ object: Any?) -> Bool {
-        return (name == (object as? CIFilter)?.name) == true
-    }
-    
-    private func filterAttribute(with key: String) -> CIFilterAttributes? {
-        return filterAttributes[key]
-    }
-    
-    func setFilterAttribute(_ value: Float, with key: String, at index: Int) {
-        filterAttribute(with: key)?.setAttributes(value: value, at: index)
-    }
-    
-    @objc dynamic var inputImage : CIImage?
-    
-    override var outputImage: CIImage? {
-        return autoreleasepool { () -> CIImage? in
-            guard let image = inputImage else { return nil }
-            filter.setValue(image, forKey: kCIInputImageKey)
-            filterAttributes.forEach { filter.setValue($0.value.value, forKey: $0.value.key) }
-            return filter.outputImage
-        }
-    }
-}
-
-class CIAdjustmentFilter: CIFilter {
-    var filterAttributes = [String: CIFilterAttributes]()
-    private var builtInFilter: CIFilter?
-    private(set) var sliderInfoItems: [CIAdjustmentSliderInfo]?
-    
-    var filter: CIFilter {
-        return builtInFilter ?? self
-    }
-    
-    required init(name: String, sliderInfo: [CIAdjustmentSliderInfo]) {
-        super.init()
-        
-        self.name = name
-        self.builtInFilter = CIFilter(name: name)
-        self.filterAttributes = [:]
-        self.sliderInfoItems = sliderInfo
-        for info in sliderInfo {
-            let attributes = CIFilterAttributes(name: info.name, key: info.attributeKey)
-            attributes.setDefaults(with: filter, name: info.name, sliderRange: info.range)
-            if let userAttributeItems = info.userAttributeItems {
-                attributes.setAttributeItems(userAttributeItems)
-            }
-            filterAttributes[info.attributeKey] = attributes
-        }
-    }
-    
-    override func copy(with zone: NSZone? = nil) -> Any {
-        let copy = type(of: self).init(name: self.name, sliderInfo: [])
-        copy.filterAttributes = Dictionary(uniqueKeysWithValues: self.filterAttributes.compactMap({
-            guard let item = $0.value.copy() as? CIFilterAttributes else { return nil }
-            return ($0.key, item)
-        }))
-        return copy
-    }
-    
-    var hasChanges: Bool {
-        return filterAttributes.values.reduce(false) { $0 || $1.hasChanges }
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
     }
     
     override func isEqual(_ object: Any?) -> Bool {
