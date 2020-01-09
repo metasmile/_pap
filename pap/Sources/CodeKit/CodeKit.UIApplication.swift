f39fc447939878428d33d1303ff56596506e0301
metasmile <cyrano905@gmail.com>
2020-01-09 20:18:23 +0100

    13

--- pap/Sources/CodeKit/CodeKit.UIApplication.swift
+++ pap/Sources/CodeKit/CodeKit.UIApplication.swift
@@ -6,12 +6,20 @@
 import Foundation
 import UIKit
 
+private struct _UIApplication {
+    static var keyWindowInScenes: UIWindow?
+}
+
 extension UIApplication{
     func openSettings(completionHandler completion: ((Bool) -> Void)? = nil){
         UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: completion)
     }
 
     var keyWindowInScenes: UIWindow? {
-        UIApplication.shared.windows.first { $0.isKeyWindow }
+        let keyWindow = _UIApplication.keyWindowInScenes
+        if keyWindow == nil {
+            _UIApplication.keyWindowInScenes = UIApplication.shared.windows.first { $0.isKeyWindow }
+        }
+        return _UIApplication.keyWindowInScenes
     }
 }
